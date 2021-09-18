eebbc14035f7314cac84a77a956cc4deb9801c5a
SANDRA DE OLIVEIRA SOUZA <66983974+Sandra23U@users.noreply.github.com>
2021-09-18 10:13:58 -0300

    Create index.py

---
+++ Mapa_Sonho&Sucesso/index.py
@@ -0,0 +1,11 @@
+def título(txt):
+    print('-'*30)
+    print(txt)
+    print('-'*30)
+   
+    
+    
+#Progama Principal
+título('   DataEngineer_BancoCarrefour    ')
+título('   Data Challange   ')
+título('   Sandra de Oliveira Souza   ')
